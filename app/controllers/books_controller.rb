127b44c78c7680f6bc28d831ba133edadf6c94d1
vagrant <k.taki415@gmail.com>
2020-07-13 11:56:51 +0000

    [Fix] path for redirect to

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,9 +15,16 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id) # .id can be ommited
+      # redirect_to book_path('Ruby_on_Rails')
+      # redirect_to "/books/#{@book.id}"
+    else
+      render 'index'
+    end
   end
 
 
@@ -26,15 +33,23 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to controller: :books, action: :show
+    else
+      render 'index'
+    end
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to controller: :books, action: :index
+    else
+      render 'index'
+    end
   end
 
   private
